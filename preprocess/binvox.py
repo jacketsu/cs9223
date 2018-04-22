dfe68f42cec547e1b93a0b527356b792a64a9cb7
Yifan Sheng <shengyifan123@gmail.com>
2018-04-22 13:34:49 -0400

    refine binvox io

--- preprocess/binvox.py
+++ preprocess/binvox.py
@@ -1,21 +1,19 @@
+# Based on https://github.com/dimatura/binvox-rw-py
+
 import numpy as np
 
 
 class Voxel(object):
 
-    def __init__(self, data, dim, translate=None, scale=1.0, axis_order="xyz"):
+    def __init__(self, data, translate=None, scale=1.0):
         self.data = data
-        self.dim = dim
-        self.translate = translate if translate is not None else [0.0, 0.0, 0.0]
+        self.translate = translate or [0.0, 0.0, 0.0]
         self.scale = scale
-        assert (axis_order in ("xzy", "xyz"))
-        self.axis_order = axis_order
 
-    def clone(self):
+    def copy(self):
         data = self.data.copy()
-        dim = self.dim[:]
         translate = self.translate[:]
-        return Voxel(data, dim, translate, self.scale, self.axis_order)
+        return Voxel(data, translate, self.scale)
 
     def write(self, fp):
         write(fp, self)
@@ -32,23 +30,19 @@ def read_header(fp):
     return dim, translate, scale
 
 
-def read(fp, fix_coords=True):
+def read(fp):
     dim, translate, scale = read_header(fp)
     raw_data = np.frombuffer(fp.read(), dtype=np.uint8)
     values, counts = raw_data[::2], raw_data[1::2]
     data = np.repeat(values, counts).astype(np.bool)
     data = data.reshape(dim)
-    if fix_coords:
-        data = np.transpose(data, (0, 2, 1))
-        axis_order = "xyz"
-    else:
-        axis_order = "xzy"
-    return Voxel(data, dim, translate, scale, axis_order)
+    data = np.transpose(data, (0, 2, 1))
+    return Voxel(data, translate, scale)
 
 
 def write_header(fp, model):
     fp.write(b"#binvox 1\n")
-    fp.write("dim {}\n".format(" ".join(map(str, model.dim))).encode("ascii"))
+    fp.write("dim {}\n".format(" ".join(map(str, model.data.shape))).encode("ascii"))
     fp.write("translate {}\n".format(" ".join(map(str, model.translate))).encode("ascii"))
     fp.write("scale {}\n".format(str(model.scale)).encode("ascii"))
     fp.write(b"data\n")
@@ -56,12 +50,7 @@ def write_header(fp, model):
 
 def write(fp, model):
     write_header(fp, model)
-    if model.axis_order not in ("xzy", "xyz"):
-        raise ValueError("Unsupported voxel model axis order")
-    if model.axis_order == "xyz":
-        data_flat = np.transpose(model.data, (0, 2, 1)).flatten()
-    else:
-        data_flat = model.data.flatten()
+    data_flat = np.transpose(model.data, (0, 2, 1)).flatten()
     state = data_flat[0]
     count = 0
     for d in data_flat:
